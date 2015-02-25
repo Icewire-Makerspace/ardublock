fb4ac7fa5fb20944103d57269f248a3244768d0d
SlimTim10 <slimtim10@gmail.com>
2015-02-25 11:56:25 -0500

    Remove tinker blocks and fix digital output block

--- src/main/java/com/ardublock/translator/block/DigitalOutputBlock.java
+++ src/main/java/com/ardublock/translator/block/DigitalOutputBlock.java
@@ -6,7 +6,6 @@ import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class DigitalOutputBlock extends TranslatorBlock
 {
-	public static final String ARDUBLOCK_DIGITAL_WRITE_DEFINE = "void __ardublockDigitalWrite(int pinNumber, boolean status)\n{\npinMode(pinNumber, OUTPUT);\ndigitalWrite(pinNumber, status);\n}\n";
 	
 	public DigitalOutputBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
 	{
@@ -16,33 +15,17 @@ public class DigitalOutputBlock extends TranslatorBlock
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
 		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
-		if (translatorBlock instanceof NumberBlock)
-		{
-			String number = translatorBlock.toCode();
-			String setupCode = "pinMode( " + number + " , OUTPUT);";
-			translator.addSetupCommand(setupCode);
+		String number = translatorBlock.toCode();
+		String setupCode = "pinMode( " + number + " , OUTPUT);";
+		translator.addSetupCommand(setupCode);
 			
-			String ret = "digitalWrite( ";
-			ret = ret + number;
-			ret = ret + " , ";
-			translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
-			ret = ret + translatorBlock.toCode();
-			ret = ret + " );\n";
-			return ret;
-		}
-		else
-		{
-			translator.addDefinitionCommand(ARDUBLOCK_DIGITAL_WRITE_DEFINE);
-			String ret = "__ardublockDigitalWrite(";
-			
-			ret = ret + translatorBlock.toCode();
-			ret = ret + ", ";
-			translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
-			ret = ret + translatorBlock.toCode();
-			ret = ret + ");\n";
-			return ret;
-		}
-		
+		String ret = "digitalWrite( ";
+		ret = ret + number;
+		ret = ret + " , ";
+		translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+		ret = ret + translatorBlock.toCode();
+		ret = ret + " );\n";
+		return ret;		
 	}
 
 }
